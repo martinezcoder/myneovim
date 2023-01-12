7c76d1b55fec83bc216cc45c743fbbd87189c43a
Fran Martinez <martinezcoder@gmail.com>
2023-01-12 11:34:55 +0100

    Add diagnostics keymaps

--- lua/default-init.lua
+++ lua/default-init.lua
@@ -31,12 +31,5 @@ require('gitsigns').setup {
   },
 }
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugin-setup/lsp/init.lua
+++ lua/plugin-setup/lsp/init.lua
@@ -6,3 +6,10 @@ end
 require "plugin-setup.lsp.mason"
 require("plugin-setup.lsp.handlers").setup()
 require "plugin-setup.lsp.null-ls"
+
+
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
