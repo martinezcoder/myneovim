a612286a49ccfa83400c077b01afc996cba74aca
Fran Martinez <martinezcoder@gmail.com>
2025-03-18 10:51:40 +0100

    Add feedkeys because of having an US keyboard with ES configuration

--- lua/plugin-setup/neotest.lua
+++ lua/plugin-setup/neotest.lua
@@ -4,6 +4,14 @@ require("neotest").setup({
   },
 })
 
+function _G.handle_ce_left()
+  vim.api.nvim_feedkeys("<", "n", true)
+end
+
+function _G.handle_ce_right()
+  vim.api.nvim_input(">")
+end
+
 local map = vim.api.nvim_set_keymap
 local options = { noremap = true }
 map('n', '<Leader>T', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>', options)
@@ -11,3 +19,6 @@ map('n', '<Leader>tt', ':lua require("neotest").run.run()<CR>', options)
 map('n', '<Leader>to', ':lua require("neotest").output.open()<CR>', options)
 map('n', '<Leader>ta', ':lua require("neotest").run.attach()<CR>', options)
 map('n', '<Leader>ts', ':lua require("neotest").summary.open()<CR>', options)
+
+map('i', 'çç', [[<Esc>:lua handle_ce_right()<CR>a]], options)
+map('i', 'ññ', [[<Esc>:lua handle_ce_left()<CR>a]], options)
